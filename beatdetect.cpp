ccf1db47d50222b70bf7be220f51734e3c83288d
Ben XO <ben@BigMac-6.local>
2019-07-14 10:29:57 +0100

    Make beat detect interrupt 25% faster by hand crafting it.

--- beatdetect.cpp
+++ beatdetect.cpp
@@ -16,8 +16,21 @@ void setup_beatdetect() {
 }
 
 ISR (INT1_vect, ISR_ALIASOF(INT0_vect));
-ISR (INT0_vect)
+ISR (INT0_vect, ISR_NAKED)
 {
     /* interrupt code here */
-    beats_from_interrupt = PIND & ((1 << BEAT_PIN_1) | (1 << BEAT_PIN_2)); // read once - port is volatile
+// beats_from_interrupt = PIND & ((1 << BEAT_PIN_1) | (1 << BEAT_PIN_2)); // read once - port is volatile
+
+  asm volatile( "push    r16                              \n\t");
+  asm volatile( "in      r16, __SREG__                    \n\t");
+  asm volatile( "push    r16                              \n\t");
+
+  asm volatile( "in      r16, %0  ;                       \n\t" :: "I" (_SFR_IO_ADDR(PIND)));
+  asm volatile( "andi    r16, %0  ;                       \n\t" :: "I" ((1 << BEAT_PIN_1) | (1 << BEAT_PIN_2)));
+  asm volatile( "sts     %0, r16  ; beats_from_interrupt  \n\t" :: "X" ((uint8_t)_SFR_MEM_ADDR(beats_from_interrupt)));
+         
+  asm volatile( "pop     r16                              \n\t");
+  asm volatile( "out     __SREG__, r16                    \n\t");
+  asm volatile( "pop     r16                              \n\t");
+  asm volatile( "reti                                     \n\t");
 }
