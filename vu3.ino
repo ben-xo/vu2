09c279155b5ac32496563c26e9c75bf5d0f5fa6b
Ben XO <ben@BigMac-6.local>
2019-07-20 19:37:44 +0100

    Add comment

--- vu3.ino
+++ vu3.ino
@@ -75,6 +75,7 @@ static void inline reach_target_fps() {
   uint32_t end_time = micros();
 #ifdef FRAME_RATE_LIMIT
   uint32_t total_time = end_time - start_time;
+  // XXX: for some reason the frame rate isn't stable. This stabilises it, even though it's the wrong FPS
   last_delay = (FRAME_LENGTH_MICROS - total_time) + (last_delay/2);
   delayMicroseconds(last_delay);
 //  Serial.println(total_time);
