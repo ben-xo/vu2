7c433da1205d5d349c47fad2e236b22fce3d52e2
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2021-07-10 02:25:16 +0100

    asmerize the fps stuff as gcc-11 actually does a worse job

--- ledpwm.cpp
+++ ledpwm.cpp
@@ -38,6 +38,49 @@
 static DigitalPin<BEAT_PIN_1> beat_pin;
 static DigitalPin<BEAT_PIN_2> tempo_pin;
 
+void __inline__ fps_count()
+{
+
+  static int8_t fps_interrupt_count = PWM_LED_FRQ / FPS;
+  volatile uint8_t* fic = &fps_interrupt_count; 
+  // // using an intermediate variable makes the compiled output much more efficient.
+  // int8_t new_interrupt_count = fps_interrupt_count - 1;
+  // if(!new_interrupt_count) {
+  //   GPIOR0 |= (1<<1);
+  //   new_interrupt_count = interrupt_reset_val;
+  // }
+  // fps_interrupt_count = new_interrupt_count;
+
+  asm volatile( 
+    "lds r24, %[fic]            \n\t" 
+    "subi  r24, 0x01            \n\t" 
+    "brne  .+4            \n\t" 
+    "sbi %0, 1            \n\t" 
+    "ldi r24, %1        \n\t" 
+    "sts %[fic], r24        \n\t" 
+    : 
+    : 
+    "I" (_SFR_IO_ADDR(GPIOR0)),
+    "M" (INTERRUPT_RESET_VAL),
+    [fic] "i" (fic)
+
+  );
+
+  // // using an intermediate variable makes the compiled output much more efficient.
+  // int8_t new_interrupt_count = fps_interrupt_count - 1;
+  //   156c: 80 91 00 01   lds r24, 0x0100 ; 0x800100 <fps_interrupt_count>
+  //   1570: 81 50         subi  r24, 0x01 ; 1
+  // if(!new_interrupt_count) {
+  //   1572: 11 f4         brne  .+4       ; 0x1578 <__vector_7+0x1a>
+  //   GPIOR0 |= (1<<1);
+  //   1574: f1 9a         sbi 0x1e, 1 ; 30
+  //   new_interrupt_count = interrupt_reset_val;
+  //   1576: 82 e4         ldi r24, 0x42 ; 66
+  // }
+  // fps_interrupt_count = new_interrupt_count;
+  //   1578: 80 93 00 01   sts 0x0100, r24 ; 0x800100 <fps_interrupt_count>  
+}
+
 void setup_ledpwm() {
   cli();
   // Clear registers
@@ -75,7 +118,6 @@ void enable_ledpwm() {
 //  TCCR2B = (1 << CS22); // re-enable the timer (with pre-scaler 64) - change PWM_PRESCALER if you change this
 }
 
-int8_t fps_interrupt_count = PWM_LED_FRQ / FPS;
 
 ISR(TIMER2_COMPA_vect, ISR_NAKED) {
   asm volatile( "push    r24                             \n\t");
@@ -93,7 +135,7 @@ ISR(TIMER2_COMPA_vect, ISR_NAKED) {
   // unfortunately we need to backup SREG for fps_count
   asm volatile( "push    r25                             \n\t");
   asm volatile( "in      r25, %0                         \n\t" :: "I" (_SFR_IO_ADDR(SREG)));
-  fps_count(PWM_LED_FRQ / FPS);
+  fps_count();
   asm volatile( "out     %0, r25                         \n\t" :: "I" (_SFR_IO_ADDR(SREG)));
   asm volatile( "pop     r25                             \n\t");
 
