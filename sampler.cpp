41f5aa2d3750c94b4ab99969fda46b6273cdc66c
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2021-07-12 21:57:00 +0100

    Only do sampler prologue when it's the sampler's turn

--- sampler.cpp
+++ sampler.cpp
@@ -45,17 +45,17 @@ void setup_sampler(uint16_t timer_counter) {
   ;
 
   // TIMER 1 for interrupt frequency 5000 Hz:  
-  TCCR1A = 0; // set entire TCCR1A register to 0
-  TCCR1B = 0; // same for TCCR1B
-  TCNT1  = 109; // initialize counter value to 0
-  // set compare match register correctly (passed in)
-  OCR1A = timer_counter;
-  // turn on CTC mode
-  TCCR1B |= (1 << WGM12);
-  // Set CS12, CS11 and CS10 bits for 1 prescaler
-  TCCR1B |= (0 << CS12) | (0 << CS11) | (1 << CS10);
-  // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
+  // TCCR1A = 0; // set entire TCCR1A register to 0
+  // TCCR1B = 0; // same for TCCR1B
+  // TCNT1  = 109; // initialize counter value to 0
+  // // set compare match register correctly (passed in)
+  // OCR1A = timer_counter;
+  // // turn on CTC mode
+  // TCCR1B |= (1 << WGM12);
+  // // Set CS12, CS11 and CS10 bits for 1 prescaler
+  // TCCR1B |= (0 << CS12) | (0 << CS11) | (1 << CS10);
+  // // enable timer compare interrupt
+  // TIMSK1 |= (1 << OCIE1A);
 
 #ifdef DEBUG_SAMPLE_RATE
   // debugging pin for checking sample rate
