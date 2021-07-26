033dc27aeed8571bbc966a5819e4f04feab0fd74
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2021-07-26 22:57:37 +0100

    re-enable startup effect

--- startup.cpp
+++ startup.cpp
@@ -5,42 +5,42 @@
 #include "effects.h"
 
 void do_banner() {
-    // // colour test
-    // for (uint16_t i = 0; i <= 255; i += STRIP_LENGTH/30) {
-    //   for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
-    //     CRGB c = Wheel((j + (i/4)) * 255 / STRIP_LENGTH);
-    //     leds[j].setRGB(c.r * i / 255, c.g * i / 255, c.b * i / 255);
-    //   }
-    //   FastLED.show();
-    // }
+    // colour test
+    for (uint16_t i = 0; i <= 255; i += STRIP_LENGTH/30) {
+      for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
+        CRGB c = Wheel((j + (i/4)) * 255 / STRIP_LENGTH);
+        leds[j].setRGB(c.r * i / 255, c.g * i / 255, c.b * i / 255);
+      }
+      FastLED.show();
+    }
 
-    // for (int16_t k = 255; k > -1; k -= STRIP_LENGTH/30) {
-    //   for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
-    //     CRGB c = Wheel((j + (STRIP_LENGTH-k)/4) * 255 / STRIP_LENGTH);
-    //     leds[j].setRGB(c.r * k / 255, c.g * k / 255, c.b * k / 255);
-    //   }
-    //   FastLED.show();
-    // }
-    // FastLED.delay(100);
+    for (int16_t k = 255; k > -1; k -= STRIP_LENGTH/30) {
+      for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
+        CRGB c = Wheel((j + (STRIP_LENGTH-k)/4) * 255 / STRIP_LENGTH);
+        leds[j].setRGB(c.r * k / 255, c.g * k / 255, c.b * k / 255);
+      }
+      FastLED.show();
+    }
+    FastLED.delay(100);
 
-    // // double flash
-    // for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
-    //     leds[j].setRGB(255,255,255);
-    // }
-    // FastLED.show();
-    // FastLED.delay(50);
+    // double flash
+    for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
+        leds[j].setRGB(255,255,255);
+    }
+    FastLED.show();
+    FastLED.delay(50);
 
-    // FastLED.clear();
-    // FastLED.show();
-    // FastLED.delay(50);
+    FastLED.clear();
+    FastLED.show();
+    FastLED.delay(50);
 
-    // for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
-    //     leds[j].setRGB(255,255,255);
-    // }
-    // FastLED.show();
-    // FastLED.delay(50);
+    for (uint8_t j = 0; j < STRIP_LENGTH; j++) {
+        leds[j].setRGB(255,255,255);
+    }
+    FastLED.show();
+    FastLED.delay(50);
 
-    // FastLED.clear();
-    // FastLED.show();
-    // FastLED.delay(100);
+    FastLED.clear();
+    FastLED.show();
+    FastLED.delay(100);
 }
