45a9052b56d80fad05849f819abf4f8c1c0edd70
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2022-01-06 22:20:28 +0000

    Helpful comments in build-and-upload.sh which I will no doubt find useful in the future when I have forgotten what I'm doing

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-set +ex
-arduino-cli compile -v --fqbn arduino:avr:nano .
+set +exuo pipefail
+
+CLEAN=
+if [[ "$1" == "--clean" ]]; then
+  # use this option if you switch avr-gcc versions with e.g. brew unlink avr-gcc@8 && brew link --force avr-gcc@11
+  # note that you'd have to set compiler.path=/usr/local/bin/ in platform.txt (see output of arduino-cli for the location)
+  CLEAN="--clean"
+fi
+
+arduino-cli compile -v $CLEAN --fqbn arduino:avr:nano .
 BOARD=$(arduino-cli board list | grep usbserial | awk '{print $1}' );
 if [[ "$BOARD" == "" ]]; then
   echo "No board found."
