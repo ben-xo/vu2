f4477668ac4dd34c6027d9e61b69dfb12d6f21a3
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2022-06-04 01:22:24 +0100

    Dead code removal

--- buttons.cpp
+++ buttons.cpp
@@ -9,33 +9,6 @@ bool is_down = false;
 uint8_t clicks = 0;
 uint16_t last_push = 0;
 
-
-// returns true on the falling edge of a button push
-// uint8_t was_button_pressed(uint8_t pins) {
-//   uint16_t now = (uint16_t) millis();
-
-//   if(is_down && !pins) {
-//     is_down = false;
-//     if(now - last_push > 3000) {
-//       // long push
-//       return LONG_PUSH;
-//     }
-//     // short push
-//     return SHORT_PUSH;
-//   }
-//   if(!is_down && pins) {
-//     is_down = true;
-//     last_push = now;
-//   }
-//   // no push yet, although one may be in progress.
-//   return NO_PUSH;
-// }
-
-// void reset_button_state() {
-//   is_down = false;
-//   last_push = (uint16_t) millis();
-// }
-
 uint8_t was_button_pressed(uint8_t pins) {
 
   uint16_t now = (uint16_t) millis();
