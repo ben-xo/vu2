cbde067f3c06d63c91c649b52e14ab1558ef463f
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2022-06-20 09:38:37 +0100

    Well, EVERY_N_MILLISECONDS is nice and all but uses a lot of static RAM

--- demo.cpp
+++ demo.cpp
@@ -93,6 +93,9 @@ static void _demo_loop(const uint8_t start_sober)
   uint8_t mode = start_sober ? 13 : 4;
   uint8_t render_mode = start_sober;
 
+  uint8_t gHue_divider = 20 * FPS / 1000; // every 20ms = every 3 frames at 150FPS
+  uint8_t gHue_counter = gHue_divider;
+
   portb_val = 0;
   portb_mask = 0;
 
@@ -154,7 +157,11 @@ static void _demo_loop(const uint8_t start_sober)
       FastLED.show();
 
       // do some periodic updates
-      EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+      if(--gHue_counter == 0) {
+        gHue_counter = gHue_divider;
+        gHue++;
+      }
+      // EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
       EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
 
       // EVERY_N_MILLISECONDS( 80 ) {
@@ -165,33 +172,31 @@ static void _demo_loop(const uint8_t start_sober)
       //   portb_val = seven_seg(mode == 7 ? 4 : mode + 1) | (seven_seg(mode) << 4);
       // }
 
-      EVERY_N_MILLISECONDS( 50 ) {
-        switch (portb_mask) {
-          case 0b00000000: portb_mask = 0b00000001; break;
-          case 0b00000001: portb_mask = 0b00010001; break;
-          case 0b00010001: portb_mask = 0b00010101; break;
-          case 0b00010101: portb_mask = 0b01010101; break;
-          case 0b01010101: portb_mask = 0b01010111; break;
-          case 0b01010111: portb_mask = 0b01110111; break;
-          case 0b01110111: portb_mask = 0b01111111; break;
-          case 0b01111111: portb_mask = 0b11111111; break;
-          case 0b11111111: portb_mask = 0b11111110; break;
-          case 0b11111110: portb_mask = 0b11101110; break;
-          case 0b11101110: portb_mask = 0b10101110; break;
-          case 0b10101110: portb_mask = 0b10101010; break;
-          case 0b10101010: portb_mask = 0b00101010; break;
-          case 0b00101010: portb_mask = 0b00100010; break;
-          case 0b00100010: portb_mask = 0b00000010; break;
-          default: 
-          case 0b00000010: 
-            mode++;
-            if(mode > 7) {
-              mode = 4;
-            }
-            portb_mask = 0b00000000;
-            portb_val = (seven_seg(mode == 7 ? 4 : mode + 1) << 4) | seven_seg(mode);
-            break;
-        }
+      switch (portb_mask) {
+        case 0b00000000: portb_mask = 0b00000001; break;
+        case 0b00000001: portb_mask = 0b00010001; break;
+        case 0b00010001: portb_mask = 0b00010101; break;
+        case 0b00010101: portb_mask = 0b01010101; break;
+        case 0b01010101: portb_mask = 0b01010111; break;
+        case 0b01010111: portb_mask = 0b01110111; break;
+        case 0b01110111: portb_mask = 0b01111111; break;
+        case 0b01111111: portb_mask = 0b11111111; break;
+        case 0b11111111: portb_mask = 0b11111110; break;
+        case 0b11111110: portb_mask = 0b11101110; break;
+        case 0b11101110: portb_mask = 0b10101110; break;
+        case 0b10101110: portb_mask = 0b10101010; break;
+        case 0b10101010: portb_mask = 0b00101010; break;
+        case 0b00101010: portb_mask = 0b00100010; break;
+        case 0b00100010: portb_mask = 0b00000010; break;
+        default: 
+        case 0b00000010: 
+          mode++;
+          if(mode > 7) {
+            mode = 4;
+          }
+          portb_mask = 0b00000000;
+          portb_val = (seven_seg(mode == 7 ? 4 : mode + 1) << 4) | seven_seg(mode);
+          break;
       }
 
     }
