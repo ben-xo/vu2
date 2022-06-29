2ea7cb6ec9667e6933d71f917cc4375a73bae444
Ben XO <75862+ben-xo@users.noreply.github.com>
2022-06-29 09:24:46 +0100

    More separation of concerns

--- fps_constants.h
+++ fps_constants.h
@@ -6,14 +6,7 @@
 #define _FPS_CONSTANTS_H
 
 #include "config.h"
-
-#define PWM_PRESCALER 8 // must match what enable_ledpwm() does
-
-// e.g. at 16MHz, overflow val will be 200. At 20Mhz, 250. At 8MHz, 100.
-#define PWM_OVERFLOW_VALUE (F_CPU / PWM_LED_FRQ / PWM_PRESCALER)
-
-// e.g. at 16MHz and 10% duty, this will be 180. At 20Mhz, 225. At 8MHz, 90.
-#define PWM_DUTY_VALUE     (PWM_OVERFLOW_VALUE - (PWM_OVERFLOW_VALUE / (100 / PWM_DUTY_PERCENT)))
+#include "pwm_constants.h"
 
 #define FPS_INTERRUPT_RESET_VAL (PWM_LED_FRQ / FPS)
 
---
+++ pwm_constants.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright Ben XO https://github.com/ben-xo All rights reserved.
+ */
+
+#ifndef _PWM_CONSTANTS_H
+#define _PWM_CONSTANTS_H
+
+#include <Arduino.h>
+#include "config.h"
+
+// These values are specific to the LEDs you choose. 
+// Override them in config.h if you like.
+
+#ifndef PWM_LED_FRQ
+#  define PWM_LED_FRQ 10000 // 10kHz
+#endif
+
+#ifndef PWM_DUTY_PERCENT
+#  define PWM_DUTY_PERCENT 10
+#endif
+
+#define PWM_PRESCALER 8 // must match what enable_ledpwm() does
+
+// e.g. at 16MHz, overflow val will be 200. At 20Mhz, 250. At 8MHz, 100.
+#define PWM_OVERFLOW_VALUE (F_CPU / PWM_LED_FRQ / PWM_PRESCALER)
+
+// e.g. at 16MHz and 10% duty, this will be 180. At 20Mhz, 225. At 8MHz, 90.
+#define PWM_DUTY_VALUE     (PWM_OVERFLOW_VALUE - (PWM_OVERFLOW_VALUE / (100 / PWM_DUTY_PERCENT)))
+
+/* definition to expand macro then apply to pragma message */
+/* from https://stackoverflow.com/questions/1562074/how-do-i-show-the-value-of-a-define-at-compile-time */
+#define VALUE_TO_STRING(x) #x
+#define VALUE(x) VALUE_TO_STRING(x)
+#define VAR_NAME_VALUE(var) #var "="  VALUE(var)
+
+// compile time debug to see the PWM vals
+#pragma message(VAR_NAME_VALUE(PWM_OVERFLOW_VALUE))
+#pragma message(VAR_NAME_VALUE(PWM_DUTY_VALUE))
+
+#endif /* _PWM_CONSTANTS_H */
