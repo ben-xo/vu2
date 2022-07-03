3064f982d09b9aa1b3a15cb27a796cf8d910c925
Ben XO <75862+ben-xo@users.noreply.github.com>
2022-07-03 17:08:01 +0100

    Use the new status LED helper methods as they're much nicer

--- ledpwm.h
+++ ledpwm.h
@@ -3,17 +3,19 @@
  */
 
 // LED PWM using interrupts.
+
 // Makes a huge assumption that you're using the entirety of PORTB for LEDs.
+
 // Uses all three GPIOR registers for speed.
-// GPIOR0 is used as a flags register 
-// - bit 0 is used to trigger the sampler at half the PWM rate (e.g. 5kHz sampler for 10kHz PWM)
-// - but 1 is used as an overflow flag for the fps_count() to tick the FPS
+
+// GPIOR0 is used as a flags register. See gpio0.h for bit meanings
 // GPIOR1 contains the PORTB value to write
 // GPIOR2 contains the a rotating mask used to swap nibbles of GPIOR1 before writing
 
 #ifndef _LEDPWM_H
 #define _LEDPWM_H
 
+#include <Arduino.h>
 #include <DigitalIO.h>
 
 #include "render.h"
@@ -31,6 +33,44 @@ void enable_ledpwm();
 
 extern DigitalPin<BEAT_PIN_1> beat_pin;
 extern DigitalPin<BEAT_PIN_2> tempo_pin;
+
+
+static void inline __attribute__((always_inline)) set_status_leds_and_mask_within_interrupt(uint8_t new_portb_val, uint8_t new_portb_mask)
+{
+    portb_mask = new_portb_mask;
+    portb_val = new_portb_val;
+}
+
+static void inline __attribute__((always_inline)) set_status_leds_within_interrupt(uint8_t new_portb_val)
+{
+    set_status_leds_and_mask_within_interrupt(new_portb_val, MASK_RESET_VAL);
+}
+
+static void inline __attribute__((always_inline)) clear_status_leds_within_interrupt()
+{
+    set_status_leds_and_mask_within_interrupt(0, MASK_RESET_VAL);
+}
+
+static void inline __attribute__((always_inline)) set_status_leds_and_mask(uint8_t new_portb_val, uint8_t new_portb_mask)
+{
+    cli();
+    set_status_leds_and_mask_within_interrupt(new_portb_val, new_portb_mask);
+    sei();
+}
+
+static void inline __attribute__((always_inline)) set_status_leds(uint8_t new_portb_val)
+{
+    cli();
+    set_status_leds_within_interrupt(new_portb_val);
+    sei();
+}
+
+static void inline __attribute__((always_inline)) clear_status_leds()
+{
+    cli();
+    clear_status_leds_within_interrupt();
+    sei();
+}
 
 
 #endif /* _LEDPWM_H */
