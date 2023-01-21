fe3fd6ae349d8194c59c9a13036d25f2927fe6e4
Ben XO <75862+ben-xo@users.noreply.github.com>
2023-01-21 22:37:18 +0000

    Fix path

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -4,7 +4,7 @@ set +exuo pipefail
 CLEAN=
 if [[ "$1" == "--clean" ]]; then
   # use this option if you switch avr-gcc versions with e.g. brew unlink avr-gcc@8 && brew link --force avr-gcc@11
-  # note that you'd have to set compiler.path=/usr/local/bin/ in platform.txt (see output of arduino-cli for the location)
+  # note that you'd have to set compiler.path=/opt/homebrew/bin/ in platform.txt (see output of arduino-cli for the location)
   CLEAN="--clean"
 fi
 
