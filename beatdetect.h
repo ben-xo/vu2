d31ab75650792414852ad599d450799958267aa3
Ben XO <75862+ben-xo@users.noreply.github.com>
2023-01-27 16:01:20 +0000

    Add missing header protection on this somewhat obsolete header

--- beatdetect.h
+++ beatdetect.h
@@ -2,8 +2,17 @@
  * Copyright Ben XO https://github.com/ben-xo All rights reserved.
  */
 
+#ifndef _BEATDETECT_H
+#define _BEATDETECT_H
+
+#include "config.h"
 
 // external beat detect using interrupts from another source.
 
+#ifdef BEAT_WITH_INTERRUPTS
 extern volatile uint8_t beats_from_interrupt;
 void setup_beatdetect();
+#endif
+
+
+#endif /* _BEATDETECT_H */
